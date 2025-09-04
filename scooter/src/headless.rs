1b9d1e50de6b7916a42f8a43a75e3eb549c85f5a
Thomas Schafer <thomasschafer97@gmail.com>
2025-09-04 11:51:34 +0100

    Bump frep-core version

--- scooter/src/headless.rs
+++ scooter/src/headless.rs
@@ -1,11 +1,17 @@
-use frep_core::{run, validation::SearchConfiguration};
+use frep_core::{
+    run,
+    validation::{DirConfig, SearchConfig},
+};
 
-pub fn run_headless(search_config: SearchConfiguration<'_>) -> anyhow::Result<String> {
-    run::find_and_replace(search_config)
+pub fn run_headless(
+    search_config: SearchConfig<'_>,
+    dir_config: DirConfig<'_>,
+) -> anyhow::Result<String> {
+    run::find_and_replace(search_config, dir_config)
 }
 
 pub fn run_headless_with_stdin(
-    search_config: SearchConfiguration<'_>,
+    search_config: SearchConfig<'_>,
     stdin_content: &str,
 ) -> anyhow::Result<String> {
     run::find_and_replace_text(stdin_content, search_config)
